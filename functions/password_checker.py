bcc138eaf162b52d3bdbfdf45782198eef1ef47a
Sumith <agirisumith06@gmail.com>
2026-06-09 18:44:15 +0530

    Fix input assignment for password variable

--- functions/password_checker.py
+++ functions/password_checker.py
@@ -5,7 +5,7 @@ condition for the password to be valid:
 2. 8 <= length of password <= 12 
 """
 
-password = (input("Enter the password: "))
+password = input("Enter the password: ")
 
 if password.isdigit() and 8 <= len(password) <=12:
   print("Valid")
