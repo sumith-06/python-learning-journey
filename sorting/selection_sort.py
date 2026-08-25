cff4a657eba005f08d4d7e4d612cea044dfec400
sumith-06 <agirisumith06@gmail.com>
2026-08-25 21:36:31 +0530

    Add Sorting technique - selection sort

---
+++ sorting/selection_sort.py
@@ -0,0 +1,17 @@
+# Selection sort
+
+# Time complexity = O(n**2)
+# Space complexity = O(1)
+
+def selection_sort(arr):
+  n = len(arr)
+
+  for i in range(n-1):
+    min_index = i
+    for j in range(i + 1, n):
+      if arr[j] < arr[min_index]:
+        min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+  return arr
