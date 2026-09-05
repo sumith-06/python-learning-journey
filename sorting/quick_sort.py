81feeeab2a27bb3efd3838786011bb9f7a3e5f30
sumith-06 <agirisumith06@gmail.com>
2026-09-05 23:35:20 +0530

    Add sorting techniques - Quick sort

---
+++ sorting/quick_sort.py
@@ -0,0 +1,17 @@
+def quick_sort(arr):
+  if len(arr) <= 1:
+    return arr
+  pivot = arr[0]
+  left = []
+  right =[]
+
+  for i in range(1, len(arr)):
+    if arr[i] <= pivot:
+      left.append(arr[i])
+    else:
+      right.append(arr[i])
+
+  return quick_sort(left) + [pivot] + quick_sort(right)
+
+nums = [4,7,2,9,3]
+print(quick_sort(nums))
